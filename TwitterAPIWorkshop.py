87a983e23cb7be3dd9e67eff192ebbb8712bfdc3
Erich Purpur <epurpur@gmail.com>
2019-09-24 10:56:50 -0400

    Add files via upload

--- TwitterAPIWorkshop.py
+++ TwitterAPIWorkshop.py
@@ -7,59 +7,110 @@ Created on Mon Sep 23 15:09:10 2019
 """
 
 from twython import Twython
+import tweepy
 import json
 import pandas as pd
-
-#consumer_key = 'your consumer key as string here'         #Copy and paste your API consumer key here. This is NOT secure and is only being used as an example
-#consumer_secret = 'your consumer secret as string here'   #Copy and paste your API consumer secret here. This is NOT secure and is only being used as an example
-consumer_key = 'iJGWsGU50hpgpHPufeqe7IoAq'
-consumer_secret = 'wj0YmifZhPH5TaY5iGCTDpQGp4rivmFq5DYkPOoegkOLAXsigD'
+from datetime import date
 
 
-python_tweets = Twython('iJGWsGU50hpgpHPufeqe7IoAq', 'wj0YmifZhPH5TaY5iGCTDpQGp4rivmFq5DYkPOoegkOLAXsigD')
-#python_tweets = Twython(f"{consumer_key}, {consumer_secret}")
 
 
-#Create your query
-query = {'q': 'University of Virginia',
-         'result_type': 'popular',
-         'count': 5,
-         'lang': 'en'
-         }
+############################### Twython
+#First you need to install Twython. In the terminal(Mac) or Shell(Windows)... pip install twython
 
-results_dict = python_tweets.search(**query)            #creates a DICTIONARY with results
+#twython_key = 'your consumer key as string here'                          #Copy and paste your API consumer key here. This is NOT secure and is only being used as an example
+#twython_secret = 'your consumer secret as string here'                    #Copy and paste your API consumer secret here. This is NOT secure and is only being used as an example
+#twython_key = 'iJGWsGU50hpgpHPufeqe7IoAq'
+#twython_secret = 'wj0YmifZhPH5TaY5iGCTDpQGp4rivmFq5DYkPOoegkOLAXsigD'
+#
+#
+#python_tweets = Twython(twython_key, twython_secret)                      #This packages your API keys together in a format that Twitter likes
+#
+#
+##Create your query
+#query = {'q': 'University of Virginia',
+#         'result_type': 'popular',
+#         'count': 5,
+#         'lang': 'en'
+#         }
+#
+#
+#results_dict = python_tweets.search(**query)            #creates a DICTIONARY with results          
+#
 #for keys, values in results_dict.items():               
 #    print(keys)                                         #only two keys (statuses, search metadata)
-    
-    
+#    
+#    
 #print(results_dict['statuses'])                         #looks for values in the 'statuses' key of results_dict. Dictionaries are unordered data types and you find the values by calling the name of each key
-
+#
+#
 #first_result = results_dict['statuses'][0]             #within ['statuses'] is a list (of dictionaries). Lists are ordered data types and you can use indexing to parse through the list elements
 #print(first_result)
-
-
-
+#
+#
 #for keys, values in first_result.items():              #first_result is now a dictionary. To see all the keys it has, do this again
 #    print(keys)
-
+#
+#
 #print(first_result['text'])                             #prints actual text of the tweet (with URL to original tweet!)
-
-
+#
+#
 #print(first_result['user'])                             #gives a bunch of information about the user who tweeted (I assume) in another DICTIONARY
 #print(first_result['user']['name'])                     #again, access the values of the keys in this dictionary 
-
-#Some batch operations:
-statuses = results_dict['statuses']                     
-for item in statuses:
-    print(item['text'])
-    print()
+#
+#
+##A batch operation:
+#statuses = results_dict['statuses']                     
+#for status in statuses:
+#    print(status['text'])
+#    print()
     
-#strip the url out of each tweet
-list_of_urls = []
 
 
 
+############################### Tweepy
+#First you need to install Twython. In the terminal(Mac) or Shell(Windows)... pip install tweepy
+
+#tweepy_key = 'your consumer key as string here'                          #Copy and paste your API consumer key here. This is NOT secure and is only being used as an example
+#tweepy_secret = 'your consumer secret as string here'                    #Copy and paste your API consumer secret here. This is NOT secure and is only being used as an example
+#tweepy_access_token = 'your access token as string here'                 #Copy and paste your API access token. This is NOT secure and is only being used as an example
+#tweepy_access_secret = 'your access token secret as string here'         #Copy and paste your API access token secret here. This is NOT secure and is only being used as an example
 
 
+#tweepy_key = 'iJGWsGU50hpgpHPufeqe7IoAq'
+#tweepy_secret = 'wj0YmifZhPH5TaY5iGCTDpQGp4rivmFq5DYkPOoegkOLAXsigD'
+#tweepy_access_token = '968923461031747584-7VxWwIwEQdU1jEcxyjB3E5vpc2oFrQL'
+#tweepy_access_secret = 'urEMTbfqDLOO5eYXcCvcAtspXy4TV7PT0lDZrYy8Y7rzC'
+#
+#auth = tweepy.OAuthHandler(tweepy_key, tweepy_secret)
+#auth.set_access_token(tweepy_access_token, tweepy_access_secret)
+#
+#api = tweepy.API(auth)
+#
+##This example will download your home timeline tweets and print the text to the console
+##public_tweets = api.home_timeline()                                     #public_tweets is a Status object                        
+##for tweet in public_tweets:
+##    print(tweet.text)
+##    print()
+#
+##Work with your own Twitter metadata
+#user = api.get_user("PurpurErich")
+#print(user.name)
+#print()
+#
+#print("My Followers")
+#for follower in user.followers():
+#    print(follower.name)
+    
 
+
+#I've use the datetime module to make a simple tweet on my twitter account.
+#api.update_status(f"Hello from Tweepy on {date.today()}")                                  #makes a status update to your twitter page. You can make a twitter bot using either Tweepy or Twython
+
+#You can build much more sophisticated twitter bots. You can watch for Twitter activity for various hashtags, follow people, automatic retweets, etc
+#To see code examples, go here: https://realpython.com/twitter-bot-python-tweepy/
+
+    
+
+###############################
 
